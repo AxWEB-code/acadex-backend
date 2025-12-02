d3bbc1c96f10808c74e092f81d4e7592c05b473e
AxWEB <axweb.code@gmail.com>
2025-12-02 22:43:23 +0100

    hj

--- src/api/superadmin/exams/exam.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createExam } from "../../../controllers/createExam.controller.old";
-
-const router = Router();
-
-router.post("/", createExam);
-
-export default router;
